f3ef5845983f214309983a6f17e098b56dbfd948
Anil Duyguc <anilduyguc3535@gmail.com>
2021-11-20 13:29:36 +0300

    less

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 $(document).ready(function () {
     $("#myCarousel").carousel({
-      interval: 2000,
-    });
-    $("#carouselButton").on("click", function () {
-      if ($("#carouselButton").children("span").hasClass("fa-pause")) {
-        $("#myCarousel").carousel("pause");
-        $("#carouselButton").children("span").removeClass("fa-pause");
-        $("#carouselButton").children("span").addClass("fa-play");
-      } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
-        $("#myCarousel").carousel("cycle");
-        $("#carouselButton").children("span").removeClass("fa-play");
-        $("#carouselButton").children("span").addClass("fa-pause");
-      }
+      interval: 20000,
     });
     $("#loginButton").on("click", function () {
       $("#loginModal").modal("show");
-    });
-    $("#tableButton").on("click", function () {
-      $("#reserveTable").modal("show");
     });
   });
   
