4d3cfac7a240095058fb63cca2f5ca25d0bb645b
Anil Duyguc <anilduyguc3535@gmail.com>
2021-11-20 14:40:47 +0300

    npm scripts part-2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-$(document).ready(function () {
-    $("#myCarousel").carousel({
-      interval: 20000,
-    });
-    $("#loginButton").on("click", function () {
-      $("#loginModal").modal("show");
-    });
+$(function () {
+  $("#myCarousel").carousel({
+    interval: 20000,
   });
-  
+  $("#loginButton").on("click", function () {
+    $("#loginModal").modal("show");
+  });
+});
